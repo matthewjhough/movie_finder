28de4230edee373dadb8d5a83b0d948d846c8e89
MatthewJHough <matthough1@icloud.com>
2017-10-23 22:05:21 -0400

    app store, app actions, and app constraint templates

---
+++ src/scripts/stores/AppStore.js
@@ -0,0 +1,34 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var AppConstants = require('../constants/AppConstants');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var AppApi = require('../utils/appApi');
+
+var CHANGE_EVENT = 'change';
+
+var _movies = [];
+var _selected = '';
+
+var AppStore = assign({}, EventEmitter.prototype, {
+    emitChange() {
+        this.emit(CHANGE_EVENT);
+    },
+    addChangeListener(callback) {
+        this.on('change', callback);
+    },
+    removeChangeListener(callback) {
+        this.removeListener('change', callback);
+    }
+});
+
+AppDispatcher.register((payload) => {
+    var action = payload.action;
+
+    switch(action.actionType) {
+
+    }
+
+    return true;
+});
+
+module.exports = AppStore;
